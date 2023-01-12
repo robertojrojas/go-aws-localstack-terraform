8de3e70929d5f7af8b45abcb7f34f61be5fe7fe8
Roberto J Rojas <robertojrojas@gmail.com>
2023-01-11 20:09:41 -0500

    removes temp files
    Signed-off-by: Roberto J Rojas <robertojrojas@gmail.com>

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -4,5 +4,8 @@ cd ../terraform/dynamodb
 terraform destroy --auto-approve
 rm -rf .terraform* terraform*
 
+cd ../../docker
+rm -rf docker.sock volume
+
 cd -
 docker-compose -f ../docker/localstack.yml down
